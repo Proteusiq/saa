b5446402234cf85d35389b8dde8224acc9b653f6
Prayson W. Daniel <praysonpi@gmail.com>
2023-06-15 22:28:07 +0200

    test for main caller

---
+++ tests/test_watch.py
@@ -0,0 +1,23 @@
+from datetime import time, datetime
+import pytest
+from saa import Watch
+
+test_cases = [
+    (time(hour=13, minute=30), "da", "halvto"),
+    ("13:15", "da", "kvart over et"),
+    (datetime(day=5, month=2, year=2022, hour=13, minute=45), "da", "kvart i to"),
+    (time(hour=13, minute=45), "en", "quarter to two"),
+    (datetime(day=5, month=2, year=2022, hour=13, minute=15), "en", "quarter past one"),
+    
+]
+
+
+
+@pytest.mark.parametrize("test_input, language, test_output", test_cases)
+def test_watches(test_input, language, test_output):
+    clock = Watch(language=language)
+    assert test_output == clock(test_input)
+
+
+
+
